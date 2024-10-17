184eceab15fe0cae5945972df31d325a433f0391
kacpstach75 <kacperstachula758@gmail.com>
2024-10-17 16:26:51 +0200

    Pierwsze zmiany

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,20 @@ public class Main {
 
 
         int variably = 45;
+
         System.out.println(variably);
         System.out.println("");
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Podaj liczbe");
         int var2 = scanner.nextInt();
+
+        System.out.println("Podaj swoje nazwisko");
+        String nazwisko = scanner.nextLine();
+
+        System.out.println("Twoje imie to" + nazwisko );
         System.out.println(variably + var2);
+
 
     }
 }
