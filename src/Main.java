80d9f3fd14cfdc2d3647e138d325c5b0b9019eb9
Czarny21012212 <wiktorstachula20@gmail.com>
2024-10-17 18:39:22 +0200

    Kacpra si***ra

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        String name = "Wiktor";
 
 
         Scanner scanner = new Scanner(System.in);
-
-
 
         System.out.println("Podaj swoje nazwisko");
         String nazwisko = scanner.nextLine();
         System.out.println("Podaj swoje imie");
         String imie = scanner.nextLine();
         System.out.println("Twoje imie to " + imie +"a nazwisko to "+  nazwisko );
+
+        System.out.println("To ja " + name + " Sprawdzam co się dzieje u siostry Kacpra");
 
 
     }
