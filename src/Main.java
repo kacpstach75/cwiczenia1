c3e8301c1f3c94713d8d1de5df163181b0f22e8c
kacpstach75 <kacperstachula758@gmail.com>
2024-10-17 16:37:03 +0200

    Drugi branch

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,16 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int variably = 45;
 
-        System.out.println(variably);
-        System.out.println("");
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Podaj liczbe");
-        int var2 = scanner.nextInt();
+
 
         System.out.println("Podaj swoje nazwisko");
         String nazwisko = scanner.nextLine();
 
-        System.out.println("Twoje imie to" + nazwisko );
-        System.out.println(variably + var2);
+        System.out.println("Twoje imie to " + nazwisko );
+
 
 
     }
